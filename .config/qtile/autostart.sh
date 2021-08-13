50454592f3ae15eead697d0788e22da760304395
cryoss <n.billing@billtec.de>
2021-08-13 14:25:53 +0200

    wallpapers

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,8 +5,9 @@ sh .screenlayout/default.sh &
 festival --tts $HOME/.config/qtile/welcome_msg &
 lxsession &
 picom &
-sh /home/cryoss/Pictures/styli.sh/styli.sh -s nature &
+#sh /home/cryoss/Pictures/styli.sh/styli.sh -s nature &
 #nitrogen --restore &
+feh --randomize --bg-fill ~/Pictures/dWallpaper/ &
 /usr/bin/emacs --daemon &
 volumeicon &
 nm-applet &
