22fdb31e9bbbd0603d147310e5e76e37431545f7
cryoss <n.billing@billtec.de>
2021-08-13 22:30:09 +0200

    init laptop

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -556,8 +556,8 @@ def init_widgets_list():
 
 def init_widgets_screen1():
     widgets_screen1 = init_widgets_list()
-    del widgets_screen1[22:28] # Uncomment for Laptop
-    #del widgets_screen1[27:30] #Uncomment for Desktop
+    #del widgets_screen1[22:28] # Uncomment for Laptop
+    del widgets_screen1[27:30] #Uncomment for Desktop
     return widgets_screen1                 # Monitor 2 will display all widgets in widgets_list
 
 
