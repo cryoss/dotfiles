1d4733df88bbf32f96ffa2175a684c14db1e797f
cryoss <n.billing@billtec.de>
2021-08-20 15:11:47 +0200

    added showkeys

---
+++ .config/qtile/showkeys.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sed -n '/#START_KEYS/,/#END_KEYS/p' ~/.config/qtile/config.py | \
+    grep -e 'Key(' -e 'desc' |\
+yad --text-info --back=#282c34 --fore=#46d9ff --geometry=1200x800 --no-buttons --undecorated --borders=0 --alpha=0
