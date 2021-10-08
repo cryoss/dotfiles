0a27ec746ece439ddfefb678ca49a6395fa450b7
cryoss <n.billing@billtec.de>
2021-10-08 17:15:00 +0200

    moved VM to seperate REPO

--- VM/Win10.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-virsh start win10
-looking-glass-client -C ~/VM/lg.rc
-##-T -a yes -w 3440 -b 1440 -p 5900
--- VM/win10mem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-touch /dev/shm/looking-glass
-chown cryoss:kvm /dev/shm/looking-glass
-chmod 660 /dev/shm/looking-glass
