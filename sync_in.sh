8fad5f9034f56b10145bea14a697cd1bfb9cfb8c
cryoss <n.billing@billtec.de>
2022-06-05 21:44:22 +0200

    added init script

---
+++ sync_in.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# mkdir .config
+cp -rf ~/.config/fish .config/
+cp -rf ~/.config/picom .config/
+cp -rf ~/.config/rofi .config/
+cp -rf ~/.config/alacritty .config/
+cp -rf ~/.config/starship.toml .config/starship.toml
+
+cp -rf ~/.config/qtile/config.py .config/qtile/
+cp -rf ~/.config/qtile/autostart.sh .config/qtile/
+cp -rf ~/.config/qtile/onreload.sh .config/qtile/
+cp -rf ~/.config/qtile/showkeys.sh .config/qtile/
